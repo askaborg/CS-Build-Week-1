85574cf6e5ac0a77f2d1730d02b3c0bb93f942df
Hien Le <hienle@askaborg.com>
2020-10-15 21:18:23 -0700

    it's alive

---
+++ gol_pygame.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()
+
+xmax = 600 #Width of screen in pixels
+ymax = 600 #Height of screen in pixels
+screen = pygame.display.set_mode((xmax, ymax), 0, 24) #New 24-bit screen
+
+def evolve_cell():
+    return
+
+def count_neighbours():
+    return
+
+def make_empty_grid():
+    return
+
+def make_random_grid():
+    return
+
+def evolve():
+    return
+
+def draw_block():
+
+def main():
+
+if __name__ == '__main__':
+    main()
